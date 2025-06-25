f19556f72ccd3c9deda2acc47ac7108dd1275ea2
matthewthomasbeck <matthewthomasbeck@gmail.com>
2025-06-25 08:28:11 -0500

    Going to see if this gets login working

--- js/global.js
+++ js/global.js
@@ -275,12 +275,11 @@ function redirectToLogin() { // function to redirect to login page
 
     /***** set variables *****/
 
-    const currentPath = encodeURIComponent(window.location.pathname);
-    const clientId = 'YOUR_COGNITO_CLIENT_ID';
-    const domain = 'YOUR_DOMAIN.auth.YOUR_REGION.amazoncognito.com';
-    const redirectBase = 'https://'+domain+'/login';
-    const redirectUri = encodeURIComponent(`https://yourdomain.com/after-login?returnTo=${currentPath}`);
-    const loginUrl = `${redirectBase}?client_id=${clientId}&response_type=code&scope=email+openid+profile&redirect_uri=${redirectUri}`;
+    const returnTo = encodeURIComponent(window.location.pathname);
+    const clientId = 'YOUR_CLIENT_ID';
+    const domain = 'YOUR_DOMAIN.auth.us-east-2.amazoncognito.com'; // or us-east-1, whatever it is
+    const redirectUri = encodeURIComponent('https://www.matthewthomasbeck.com/logging_in.html?returnTo=' + returnTo);
+    const loginUrl = `https://${domain}/login?client_id=${clientId}&response_type=code&scope=email+openid+profile&redirect_uri=${redirectUri}`;
 
     /***** redirect to login page *****/
 
